93123acbecfb6ef50d51b1685704772cf856d673
Maided Ponce <maided.h.p90@gmail.com>
2021-12-20 22:02:46 -0600

    cambiando de renderizado

--- containers/Home.js
+++ containers/Home.js
@@ -1,33 +1,36 @@
 import React, { useState, useEffect } from "react";
+import fetch from "isomorphic-unfetch";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import Link from "next/link";
-const Home = () => {
-    const [productList, setProductList] = useState([]);
 
-    useEffect(() => {
-        window
-            .fetch("/api/avo")
-            .then((response) => response.json())
-            .then(({ data, length }) => setProductList(data));
-    }, []);
+export const getServerSideProps = async (p) => {
+    const res = await fetch("/api/avo");
+    const { data } = await res.json();
+    return {
+        props: {
+            productList: data,
+        },
+    };
+};
 
+const Home = ({ productList }) => {
     return (
         <section className={styles.section}>
             {productList.map((item) => (
-             <Link href={`character/${item.id}`}>
-                <div className={styles.card} key={item.id}>
-                    <figure className={styles.figure}>
-                        <Image width="190" height="190" src={item.image} />
-                    </figure>
-                    <div className={styles.details}>
-                        <span>{item.name}</span>
-                        <span>
-                            <b>{item.last}</b>
-                        </span>
-                        <span>{item.aldea}</span>
+                <Link href={`character/${item.id}`}>
+                    <div className={styles.card} key={item.id}>
+                        <figure className={styles.figure}>
+                            <Image width="190" height="190" src={item.image} />
+                        </figure>
+                        <div className={styles.details}>
+                            <span>{item.name}</span>
+                            <span>
+                                <b>{item.last}</b>
+                            </span>
+                            <span>{item.aldea}</span>
+                        </div>
                     </div>
-                </div>
                 </Link>
             ))}
         </section>
